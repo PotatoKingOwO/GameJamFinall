7958fee9cf7a96ba346ca5f8a5497d547a20b5a4
PotatoKingOwO <matyaslukes18@gmail.com>
2026-06-26 21:54:26 +0200

    AddedPlayerHealthScript

---
+++ MiniGameJam/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int playerHealth;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GetHurt()
+    {
+
+    }
+}
